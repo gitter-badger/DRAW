4bc2c60552348003bf5a33686c4ffe65e2187da6
Kartik Chopra <k5chopra@uwaterloo.ca>
2018-02-21 22:39:43 -0500

    Removed draw.ini and added multireddit test folder.
    draw.ini removal - should not have any sideeffects.
    New Multireddit folder contains empty test.

---
+++ test/multreddit/multireddit_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2017, the Dart Reddit API Wrapper project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:draw/draw.dart';
+import 'package:test/test.dart';
+
+import '../test_utils.dart';
+
+Future main() async {
+  test('lib/subreddit/banned', () async {
+    final reddit = await createRedditTestInstance(
+        'test/multireddit/lib_multireddit_banned.json', live:true);
+  });
+}
