a28ec45cdc632da0b2245fe2cd139fedc291d4ab
Kartik Chopra <k5chopra@uwaterloo.ca>
2018-02-24 23:27:45 -0500

    Fixed issues with multireddit tests.

--- test/multreddit/multireddit_test.dart
+++ test/multreddit/multireddit_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, the Dart Reddit API Wrapper project authors.
+
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
@@ -13,13 +13,11 @@ import '../test_utils.dart';
 Future main() async {
   test('lib/subreddit/banned', () async {
     final reddit = await createRedditTestInstance(
-        'test/multireddit/lib_multireddit_banned.json', live:true);
-    Map data = {
-      'name': 'drawapitesting',
+        'test/multireddit/lib_multireddit_banned.json',
+        live: true);
+    Map data = {'name': 'drawapitesting'};
 
-    Map testData = {
-      'data': data
-    }
-    final multireddit = new Multireddit.parse(reddit, );
+    Map testData = {'data': data};
+//    final multireddit = new Multireddit.parse(reddit, );
   });
 }
